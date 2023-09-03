c1e83a791f070bb1b05ddf436f39573068066e1d
priynakaK <140493065+priynakaK@users.noreply.github.com>
2023-09-03 15:03:44 +0530

    1 file modified

--- src/app/student/student-details/student-details.component.ts
+++ src/app/student/student-details/student-details.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Studentinterface } from '../studentinterface';
 
 @Component({
   standalone: true,
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
   styleUrls: ['./student-details.component.css'],
+  imports: [CommonModule],
 })
 export class StudentDetailsComponent implements OnInit {
   constructor() {}
